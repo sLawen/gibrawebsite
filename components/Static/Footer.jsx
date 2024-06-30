50407aaf23910fe0045f67bea4a4144fa1fad102
Lawen <112584771+sLawen@users.noreply.github.com>
2024-06-30 19:42:46 +0200

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -13,16 +13,6 @@ export default function Footer() {
                             <div className="flex items-center space-x-5">
                                 <p className="font-semibold text-xl text-white">Gibrabot</p>
                             </div>
-                            <Menu as="div" className="relative mt-3 inline-block text-left">
-                                <div>
-                                    <Menu.Button className="flex z-1 items-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                                        <ChevronDownIcon
-                                            className="w-5 h-5 ml-2 -mr-1 transform rotate-180 text-violet-200 hover:text-violet-100"
-                                            aria-hidden="true"
-                                        />
-                                    </Menu.Button>
-                                </div>
-                            </Menu>
                         
                         </div>
                         <div className="col-span-1">
